f4187637a1fd18827f387b61c4c3eb97f0359748
0x46641766F7572 <96478305+favourolaoye@users.noreply.github.com>
2024-09-13 16:54:16 +0100

    updates

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import search from "../public/search.png"
-import Image from 'next/image'
-export default function Search() {
+import React from "react";
+import search from "../public/search.png";
+import Image from "next/image";
+
+export default function SearchInput() {
   return (
-  <form className='flex justify-between bg-gray-100 border-2 rounded-3xl px-3'>
-    <input className='bg-transparent  outline-none p-2' type="text" placeholder='search'/>
-    <button className='bg-transparent'>
-        <Image src={search} alt='' width={20} height={20}/>
-    </button>
-  </form>
-  )
+    <div>
+      <form className="flex justify-between bg-gray-100 border-2 rounded-3xl px-3">
+        <input
+          className="bg-transparent  outline-none p-2"
+          type="text"
+          placeholder="search"
+        />
+        <button className="bg-transparent">
+          <Image src={search} alt="" width={20} height={20} />
+        </button>
+      </form>
+    </div>
+  );
 }
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 import Menu from "./Menu";
 import Image from "next/image";
 import logo from "../public/logo.png";
-import Search from "./Search";
 import NavIcons from "./NavIcons";
+import SearchInput from "./Search";
 export default function Navbar() {
   return (
   <div className="h-16 px-5 md:px-8 lg:px-16 xl:px-32 bg-gray-100 2xl:px-64 relative">
       {/* mobile screens */}
-      <div className="h-full flex justify-between items-center md:hidden">
+      <div className="md:hidden h-full flex justify-between items-center ">
         <Link href="/">
           <div className="tracking-wide text-2xl font-semibold">Ria's</div>
         </Link>
@@ -19,10 +19,10 @@ export default function Navbar() {
       <div className="hidden md:flex justify-between w-full h-16 items-center">
         <div className="flex justify-between w-10 cursor-pointer gap-1">
           <Image src={logo} alt="" height={24} width={24} className="bg-transparent"/>
-          <p className="text-2xl tracking-wide">Ria's</p>
+          <p className="text-2xl tracking-wide">Ria&apos;s</p>
         </div>
         <div className="flex items-center justify-between w-2/3">
-         <Search/>
+         <SearchInput/>
          <NavIcons/>
         </div>
       </div>
