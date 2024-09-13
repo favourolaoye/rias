67377ef8c57b9e871988fa3d3ed75b47c695f63d
0x46641766F7572 <96478305+favourolaoye@users.noreply.github.com>
2024-09-13 14:45:03 +0100

    updates

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import search from "../public/search.png"
 import Image from 'next/image'
-export default function () {
+export default function Search() {
   return (
   <form className='flex justify-between bg-gray-100 border-2 rounded-3xl px-3'>
     <input className='bg-transparent  outline-none p-2' type="text" placeholder='search'/>
